9905f2174d01c54e71e8418d5893d8a8fd3ee6f3
Alex Mikhankov <jesslich.nime@gmail.com>
2024-10-26 13:51:37 +0300

    enhance rediska features

--- src/rediska/client.rs
+++ src/rediska/client.rs
@@ -34,11 +34,12 @@ impl Rediska {
     /// #[tokio::main]
     /// async fn main() -> anyhow::Result<()> {
     ///     let config = RedisConfig {
-    ///         host: "127.0.0.1".to_string(),
-    ///         port: 6379,
+    ///         connection_url: None,
+    ///         host: Option::from("127.0.0.1".to_string()),
+    ///         port: Option::from(6379),
     ///         username: Option::from("username".to_string()),
     ///         password: Some("top_secret_password".to_string()),
-    ///         db: 0,
+    ///         db: Option::from(0),
     ///         connection_timeout: std::time::Duration::from_secs(60),
     ///         connection_pool_size: 10,
     ///     };
@@ -48,24 +49,34 @@ impl Rediska {
     /// }
     /// ```
     pub async fn new(config: RedisConfig) -> Result<Self, anyhow::Error> {
-        let connection_url = if let Some(ref username) = config.username {
-            format!(
-                "redis://{}:{}@{}:{}/{}",
-                username, config.password.clone().unwrap_or_default(),
-                config.host, config.port, config.db
-            )
-        } else if let Some(ref password) = config.password {
-            format!(
-                "redis://:{}@{}:{}/{}",
-                password, config.host, config.port, config.db
-            )
+        config.check()?;
+
+        let connection_url = if let Some(url) = &config.connection_url {
+            url.clone()
         } else {
-            format!("redis://{}:{}/{}", config.host, config.port, config.db)
+            let password_part = if let Some(ref password) = config.password {
+                format!(":{}", password)
+            } else {
+                String::new()
+            };
+            let auth_part = if let Some(ref username) = config.username {
+                format!("{}{}", username, password_part)
+            } else {
+                password_part
+            };
+            format!(
+                "redis://{}@{}:{}/{}",
+                auth_part,
+                config.host.as_ref().unwrap(),
+                config.port.unwrap(),
+                config.db.unwrap_or(0)
+            )
         };
 
         let manager = RedisConnectionManager::new(connection_url)?;
         let pool = Pool::builder()
             .max_size(config.connection_pool_size)
+            .connection_timeout(config.connection_timeout)
             .build(manager)
             .await?;
 
@@ -74,12 +85,113 @@ impl Rediska {
 }
 
 impl Rediska {
-    /// Sets a value in Redis for the given key.
+    /// Retrieves a connection from the Redis pool.
+    ///
+    /// This method provides direct access to a Redis connection, allowing developers
+    /// to execute any Redis command available in the `redis` crate. This approach offers
+    /// flexibility for performing operations not explicitly implemented in `Rediska`.
+    ///
+    /// # Examples
+    ///
+    /// ## Using `del` to delete a key:
+    ///
+    /// ```no_run
+    /// use multitool_hg::rediska::config::RedisConfig;
+    /// use multitool_hg::rediska::client::Rediska;
+    /// use redis::AsyncCommands;
+    ///
+    /// #[tokio::main]
+    /// async fn main() -> anyhow::Result<()> {
+    ///     let config = RedisConfig {
+    ///         connection_url: None,
+    ///         host: Some("127.0.0.1".to_string()),
+    ///         port: Some(6379),
+    ///         username: None,
+    ///         password: Some("top_secret_password".to_string()),
+    ///         db: Some(0),
+    ///         connection_timeout: std::time::Duration::from_secs(60),
+    ///         connection_pool_size: 10,
+    ///     };
+    ///
+    ///     let redis_client = Rediska::new(config).await?;
+    ///     let mut conn = redis_client.conn().await?;
+    ///
+    ///     let _: () = conn.del("key_to_delete").await?;
+    ///     Ok(())
+    /// }
+    /// ```
+    ///
+    /// ## Using `expire` to set a TTL on a key:
+    ///
+    /// ```no_run
+    /// use multitool_hg::rediska::config::RedisConfig;
+    /// use multitool_hg::rediska::client::Rediska;
+    /// use redis::AsyncCommands;
+    ///
+    /// #[tokio::main]
+    /// async fn main() -> anyhow::Result<()> {
+    ///     let config = RedisConfig {
+    ///         connection_url: None,
+    ///         host: Some("127.0.0.1".to_string()),
+    ///         port: Some(6379),
+    ///         username: None,
+    ///         password: Some("top_secret_password".to_string()),
+    ///         db: Some(0),
+    ///         connection_timeout: std::time::Duration::from_secs(60),
+    ///         connection_pool_size: 10,
+    ///     };
+    ///
+    ///     let redis_client = Rediska::new(config).await?;
+    ///     let mut conn = redis_client.conn().await?;
+    ///
+    ///     let _: () = conn.expire("key_with_ttl", 120).await?; // Key expires in 120 seconds
+    ///     Ok(())
+    /// }
+    /// ```
+    ///
+    /// ## Using `incr` to increment a key’s value:
+    ///
+    /// ```no_run
+    /// use multitool_hg::rediska::config::RedisConfig;
+    /// use multitool_hg::rediska::client::Rediska;
+    /// use redis::AsyncCommands;
+    ///
+    /// #[tokio::main]
+    /// async fn main() -> anyhow::Result<()> {
+    ///     let config = RedisConfig {
+    ///         connection_url: None,
+    ///         host: Some("127.0.0.1".to_string()),
+    ///         port: Some(6379),
+    ///         username: None,
+    ///         password: Some("top_secret_password".to_string()),
+    ///         db: Some(0),
+    ///         connection_timeout: std::time::Duration::from_secs(60),
+    ///         connection_pool_size: 10,
+    ///     };
+    ///
+    ///     let redis_client = Rediska::new(config).await?;
+    ///     let mut conn = redis_client.conn().await?;
+    ///
+    ///     let new_value: i64 = conn.incr("counter_key", 1).await?;
+    ///     println!("New counter value: {}", new_value);
+    ///     Ok(())
+    /// }
+    /// ```
+    ///
+    /// # Returns
+    ///
+    /// A `Result` with a pooled Redis connection if successful, or an `anyhow::Error` if the connection cannot be obtained.
+    pub async fn conn(&self) -> anyhow::Result<bb8::PooledConnection<'_, RedisConnectionManager>> {
+        self.pool.get().await.map_err(anyhow::Error::from)
+    }
+
+    /// Sets a value in Redis for the given key with an optional TTL.
     ///
     /// # Arguments
     ///
     /// * `key` - The key under which the value should be stored.
     /// * `value` - The value to store in Redis.
+    /// * `ttl` - An optional TTL in seconds. If `None`, the key will not expire.
     ///
     /// # Returns
     ///
@@ -94,23 +206,29 @@ impl Rediska {
     /// #[tokio::main]
     /// async fn main() -> anyhow::Result<()> {
     ///     let config = RedisConfig {
-    ///         host: "127.0.0.1".to_string(),
-    ///         port: 6379,
+    ///         connection_url: None,
+    ///         host: Option::from("127.0.0.1".to_string()),
+    ///         port: Option::from(6379),
     ///         username: None,
     ///         password: Some("top_secret_password".to_string()),
-    ///         db: 0,
+    ///         db: Option::from(0),
     ///         connection_timeout: std::time::Duration::from_secs(60),
     ///         connection_pool_size: 10,
     ///     };
     ///
     ///     let redis_client = Rediska::new(config).await?;
-    ///     redis_client.set("my_key", "my_value").await?;
+    ///     redis_client.set("key_with_ttl", "Key will expire in 1 hour", Some(3600)).await?;
+    ///     redis_client.set("another_key", "Key will never expire", None).await?;
     ///     Ok(())
     /// }
     /// ```
-    pub async fn set(&self, key: &str, value: &str) -> anyhow::Result<()> {
+    pub async fn set(&self, key: &str, value: &str, ttl: Option<u64>) -> anyhow::Result<()> {
         let mut conn = self.pool.get().await?;
-        let _: () = conn.set(key, value).await?;
+        if let Some(seconds) = ttl {
+            let _: () = conn.set_ex(key, value, seconds).await?;
+        } else {
+            let _: () = conn.set(key, value).await?;
+        }
         Ok(())
     }
 
@@ -132,13 +250,13 @@ impl Rediska {
     ///
     /// #[tokio::main]
     /// async fn main() -> anyhow::Result<()> {
-    ///     use multitool_hg::rediska::config::RedisConfig;
-    /// let config = RedisConfig {
-    ///         host: "127.0.0.1".to_string(),
-    ///         port: 6379,
+    ///     let config = RedisConfig {
+    ///         connection_url: None,
+    ///         host: Option::from("127.0.0.1".to_string()),
+    ///         port: Option::from(6379),
     ///         username: None,
     ///         password: None,
-    ///         db: 0,
+    ///         db: Option::from(0),
     ///         connection_timeout: std::time::Duration::from_secs(60),
     ///         connection_pool_size: 10,
     ///     };
--- src/rediska/config.rs
+++ src/rediska/config.rs
@@ -6,6 +6,10 @@ use serde::{Deserialize, Serialize};
 /// This configuration object is used to set up a connection pool to Redis, providing
 /// necessary details such as the Redis host, port, authentication credentials, and pool settings.
 ///
+/// In more advanced setups, such as Redis clusters or socket connections, the `connection_url` field
+/// can be used to provide a direct connection string. If `connection_url` is provided,
+/// fields like `host`, `port`, and `db` become optional.
+///
 /// Configuration fields can be loaded from various sources, such as YAML, JSON
 /// configuration files, or from environment variables.
 ///
@@ -23,21 +27,56 @@ use serde::{Deserialize, Serialize};
 /// connection_pool_size: 10
 /// ```
 ///
+/// Another example configuration in YAML:
+///
+/// ```yaml
+/// connection_url: "redis://username:password@localhost:6379/0"
+/// connection_timeout:
+///   secs: 60
+///   nanos: 0
+/// connection_pool_size: 10
+/// ```
+///
 /// Fields like `connection_timeout` and `connection_pool_size` control how the connection pool behaves.
 #[derive(Debug, Deserialize, Serialize)]
 pub struct RedisConfig {
-    /// The Redis server host address (e.g. localhost).
-    pub host: String,
-    /// The port to connect to the Redis server (e.g. 6379 for Redis).
-    pub port: u16,
+    /// Optional direct connection URL (useful for Redis clusters or socket connections).
+    /// If `connection_url` is specified, the `host`, `port`, and `db` fields become optional and are not used.
+    pub connection_url: Option<String>,
+    /// The Redis server host address (e.g., localhost).
+    pub host: Option<String>,
+    /// The port to connect to the Redis server (e.g., 6379 for Redis).
+    pub port: Option<u16>,
     /// The optional username to authenticate with Redis (mostly used in Redis 6+ with ACLs).
     pub username: Option<String>,
     /// The optional password to authenticate with Redis.
     pub password: Option<String>,
     /// The database number to connect to (default is 0).
-    pub db: u64,
+    pub db: Option<u64>,
     /// The timeout duration for establishing a connection to the Redis server.
     pub connection_timeout: Duration,
     /// The maximum number of connections allowed in the pool.
     pub connection_pool_size: u32,
+}
+
+impl RedisConfig {
+    /// Checks the validity of the configuration.
+    ///
+    /// If `connection_url` is provided, the configuration is considered valid.
+    /// Otherwise, `host`, `port`, and `db` must be provided for a valid configuration.
+    ///
+    /// # Returns
+    ///
+    /// `Ok(())` if the configuration is valid, or an `anyhow::Error` explaining the missing fields.
+    pub fn check(&self) -> anyhow::Result<()> {
+        if self.connection_url.is_some() {
+            Ok(())
+        } else if self.host.is_some() && self.port.is_some() && self.db.is_some() {
+            Ok(())
+        } else {
+            Err(anyhow::Error::msg(
+                "Either `connection_url` must be provided or fields `host`, `port`, and `db` must be set for Redis connection."
+            ))
+        }
+    }
 }
